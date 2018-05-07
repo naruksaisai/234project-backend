0f3dcf99433d7c175949beb5813f736788dd8222
Unknown <xiaoyudu517@gmail.com>
2018-05-07 17:57:50 +0700

    Add mock data in saleOrderServiceTest

--- src/test/java/camt/se234/project/SaleOrderServiceTest.java
+++ src/test/java/camt/se234/project/SaleOrderServiceTest.java
@@ -37,14 +37,20 @@ public class SaleOrderServiceTest {
                 new SaleOrder("S001", saleTransactions1),
                 new Product("P001", "Meat", "xxx", "xxx", 100.00), 10));
         saleTransactions1.add(new SaleTransaction("T002",
-                new SaleOrder("S001", saleTransactions1),
+                new SaleOrder("A001", saleTransactions1),
                 new Product("P002", "Donut", "xxx", "xxx", 100.00), 5));
+        saleTransactions1.add(new SaleTransaction("T003",
+                new SaleOrder("W001", saleTransactions1),
+                new Product("P003", "Banana", "xxx", "xxx", 300.00), 5));
         mockOrders.add(new SaleOrder("S001", saleTransactions1));
-        mockOrders.add(new SaleOrder("T002", saleTransactions1));
+        mockOrders.add(new SaleOrder("A001", saleTransactions1));
+        mockOrders.add(new SaleOrder("W001", saleTransactions1));
         when(saleOrderService.getSaleOrders()).thenReturn(mockOrders);
 //        assertThat(saleOrderService.getSaleOrders(),hasItem("S001", saleTransactions1, new Product("P001", "Meat", "xxx", "xxx", 100.00), 10);
         assertThat(saleOrderService.getSaleOrders(),hasItem(new SaleOrder("S001", saleTransactions1)));
-        assertThat(saleOrderService.getSaleOrders(),hasItems(new SaleOrder("S001", saleTransactions1),new SaleOrder("T002", saleTransactions1)));
+        assertThat(saleOrderService.getSaleOrders(),hasItems(new SaleOrder("S001", saleTransactions1),
+                new SaleOrder("A001", saleTransactions1),
+                new SaleOrder("W001", saleTransactions1)));
     }
     @Test
     public void testGetAverageSaleOrderPrice(){
